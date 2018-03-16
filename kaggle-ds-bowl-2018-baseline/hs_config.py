0c3ef8f4cf79646bec9b8abd59b3903bff33f79e
hooman67 <houman_sh2001@hotmail.com>
2018-03-16 21:53:54 +0000

    train works with resnet101 and with starting from last trained weights

---
+++ kaggle-ds-bowl-2018-baseline/hs_config.py
@@ -0,0 +1,48 @@
+from config import Config
+
+#HS config classes must override the config.py. bowl_config was wan extension. this is mine.
+
+class HsConfig(Config):
+    """Configuration for training on the toy shapes dataset.
+    Derives from the base Config class and overrides values specific
+    to the toy shapes dataset.
+    """
+    # Give the configuration a recognizable name
+    NAME = "hsConfig"
+
+    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
+    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 2 # hs orig was 2, 16 wokrs with resNet50 not with 101
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1 # background + nuclei
+
+    # Use small images for faster training. Set the limits of the small side
+    # the large side, and that determines the image shape.
+    IMAGE_MIN_DIM = 512 # hs chaging this to 128 failed I think
+    IMAGE_MAX_DIM = 512 # hs chaging this to 128 failed I think
+
+    # Use smaller anchors because our image and objects are small
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
+
+    # Reduce training ROIs per image because the images are small and have
+    # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
+    TRAIN_ROIS_PER_IMAGE = 600
+
+    STEPS_PER_EPOCH = None
+
+    # use small validation steps since the epoch is small
+    VALIDATION_STEPS = 5
+
+    USE_MINI_MASK = True
+
+    MAX_GT_INSTANCES = 256
+
+    DETECTION_MAX_INSTANCES = 512
+
+    RESNET_ARCHITECTURE = "resnet101"  # original was "resnet50" i tried "resnet101" got a reource exhustion. 
+
+
+hs_config = HsConfig()
+hs_config.display()
